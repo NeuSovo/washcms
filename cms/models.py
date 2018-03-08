cc7452e964e47d89ecfd808743b238b520077dc8
zzde <zhangxh1997@gmail.com>
2018-03-08 21:30:28 +0800

    add last_login

--- cms/models.py
+++ cms/models.py
@@ -13,13 +13,28 @@ class User(models.Model):
         (3, u'顾客'),
         (4, u'未注册')
     )
-    wk = models.CharField(max_length=100, unique=True, primary_key=True)
+    wk = models.CharField(
+            max_length=100,
+            unique=True,
+            primary_key=True
+        )
     user_type = models.IntegerField(
-        default=4, choices=type_level, verbose_name='用户身份')
-    nick_name = models.CharField(max_length=100, default='nick_name')
+                    default=4,
+                    choices=type_level,
+                    verbose_name='用户身份'
+                )
+    nick_name = models.CharField(
+                    max_length=100,
+                    default='nick_name'
+                )
     avatar_links = models.CharField(
-        max_length=150, default='https://pic3.zhimg.com/aadd7b895_s.jpg')
-    reg_date = models.DateTimeField(auto_now_add=True)
+                        max_length=150,
+                        default='https://pic3.zhimg.com/aadd7b895_s.jpg'
+                    )
+    reg_date = models.DateTimeField(
+                   auto_now_add=True
+                )
+    last_login = models.DateTimeField()
 
     @staticmethod
     def all_admin():
@@ -40,8 +55,8 @@ class User(models.Model):
     def __len__(self):
         return len(User.user_all())
 
-    def save(self):
-        super().save()
+    def __str__(self):
+        return '%s : %s' % (self.nick_name, self.type_level[self.user_type])
 
 
 class DeliveryArea(models.Model):
@@ -64,13 +79,30 @@ class Store(models.Model):
     # for i in DeliveryArea.area_all():
     #     area_level.append([i.id, i.area_name])
 
-    store_id = models.IntegerField(primary_key=True)
-    store_name = models.CharField(max_length=155, default=0)
-    store_phone = models.IntegerField(default=0)
-    store_addr = models.CharField(max_length=155)
-    store_area = models.IntegerField(default=0, choices=area_level)
-    store_pay_type = models.IntegerField(default=0, choices=pay_type_level)
-    store_deposit = models.IntegerField(default=0)
+    store_id = models.IntegerField(
+                    primary_key=True
+                )
+    store_name = models.CharField(
+                    max_length=155,
+                    default=0
+                )
+    store_phone = models.IntegerField(
+                    default=0
+                )
+    store_addr = models.CharField(
+                    max_length=155
+                )
+    store_area = models.IntegerField(
+                    default=0,
+                    choices=area_level
+                )
+    store_pay_type = models.IntegerField(
+                    default=0,
+                    choices=pay_type_level
+                )
+    store_deposit = models.IntegerField(
+                    default=0
+                )
 
     @staticmethod
     def store_all():
@@ -82,26 +114,50 @@ class Store(models.Model):
 
 class CustomerProfile(models.Model):
 
-    wk = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    store_id = models.IntegerField(default=-1)
+    wk = models.OneToOneField(
+                User,
+                on_delete=models.CASCADE,
+                primary_key=True
+            )
+    store_id = models.IntegerField(
+                default=-1
+            )
 
 
 class CourierProfile(models.Model):
 
-    wk = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    area_id = models.IntegerField(default=-1)
+    wk = models.OneToOneField(
+            User,
+            on_delete=models.CASCADE,
+            primary_key=True
+        )
+    area_id = models.IntegerField(
+            default=-1
+        )
 
 
 class Goods(models.Model):
     recover_level = (
-        (0,'回收'),
-        (1,'不回收')
+        (0, '回收'),
+        (1, '不回收')
     )
-    goods_id = models.AutoField(primary_key=True)
-    goods_name = models.CharField(max_length=155,default='not name')
-    goods_spec = models.IntegerField(default=1)
-    goods_stock = models.IntegerField(default=0)
-    is_recover = models.IntegerField(default=0, choices=recover_level)
+    goods_id = models.AutoField(
+                    primary_key=True
+                )
+    goods_name = models.CharField(
+                    max_length=155,
+                    default='not name'
+                )
+    goods_spec = models.IntegerField(
+                    default=0
+                )
+    goods_stock = models.IntegerField(
+                    default=0
+                )
+    is_recover = models.IntegerField(
+                    default=0,
+                    choices=recover_level
+                )
 
     @staticmethod
     def goods_all():
@@ -109,55 +165,117 @@ class Goods(models.Model):
 
 
 class StoreGoods(models.Model):
-    store_id = models.IntegerField()
-    goods_id = models.IntegerField()
-    goods_name = models.CharField(max_length=155)
-    goods_stock = models.IntegerField(default=0)
-    goods_spec = models.IntegerField(default=1)
-    goods_price = models.DecimalField(max_digits=8, decimal_places=3)
+    store_id = models.IntegerField(
+                    null=False,
+                    blank=True
+                )
+    goods_id = models.IntegerField(
+                    null=False,
+                    blank=True
+                )
+    goods_name = models.CharField(
+                    max_length=155
+                )
+    goods_stock = models.IntegerField(
+                    default=0
+                )
+    goods_spec = models.IntegerField(
+                    default=1
+                )
+    goods_price = models.DecimalField(
+                    max_digits=8,
+                    decimal_places=3
+                )
 
 
 class Order(models.Model):
     pay_from_level = (
-        (0,'现金'),
-        (1,'微信'),
-        (2,'月结'),
-        (3,'未支付')
+        (0, '现金'),
+        (1, '微信'),
+        (2, '月结'),
+        (3, '未支付')
     )
     receive_level = (
-        (0,'已送到'),
-        (1,'未送到')
+        (0, '已送到'),
+        (1, '未送到')
         )
 
-    order_id = models.BigIntegerField(primary_key=True)
-    create_time = models.DateTimeField(auto_now_add=True)
-    store_id = models.IntegerField(blank=False)
-    user_id = models.CharField(max_length=155)
-    area_id = models.IntegerField(blank=False)
-    is_receive = models.IntegerField(choices=receive_level,default=1)
-    is_pay = models.IntegerField(default=1)
-    pay_from = models.IntegerField(choices=pay_from_level,default=3)
-    order_total_price = models.DecimalField(max_digits=8, decimal_places=3)
-    order_remarks = models.CharField(max_length=155)
-    done_time = models.DateTimeField(auto_now=True)
+    order_id = models.BigIntegerField(
+                    primary_key=True
+                )
+    create_time = models.DateTimeField(
+                    auto_now_add=True
+                )
+    store_id = models.IntegerField(
+                    blank=False
+                )
+    user_id = models.CharField(
+                    max_length=155
+                )
+    area_id = models.IntegerField(
+                    blank=False
+                )
+    is_receive = models.IntegerField(
+                    choices=receive_level,
+                    default=1
+                )
+    is_pay = models.IntegerField(
+                    default=1
+                )
+    pay_from = models.IntegerField(
+                    choices=pay_from_level,
+                    default=3
+                )
+    order_total_price = models.DecimalField(
+                    max_digits=8,
+                    decimal_places=3
+                )
+    order_remarks = models.CharField(
+                    max_length=155
+                )
+    done_time = models.DateTimeField(
+                    auto_now=True
+                )
 
 
 class OrderDetail(models.Model):
     order_id = models.BigIntegerField()
     goods_id = models.IntegerField()
     goods_count = models.IntegerField()
-    goods_price = models.DecimalField(max_digits=8,decimal_places=3)
-    total_price = models.DecimalField(max_digits=8,decimal_places=3)
+    goods_price = models.DecimalField(
+                    max_digits=8,
+                    decimal_places=3
+                )
+    total_price = models.DecimalField(
+                    max_digits=8,
+                    decimal_places=3
+                )
 
 
 class Session(models.Model):
-    session_key = models.CharField(max_length=100, primary_key=True)
-    session_data = models.CharField(max_length=100, unique=True)
-    we_ss_key = models.CharField(max_length=100)
+    session_key = models.CharField(
+                    max_length=100,
+                    primary_key=True
+                )
+    session_data = models.CharField(
+                    max_length=100,
+                    unique=True
+                )
+    we_ss_key = models.CharField(
+                    max_length=100
+                )
     expire_date = models.DateTimeField()
 
 
 class CodeRecord(models.Model):
-    code_key = models.IntegerField(primary_key=True)
-    code_name = models.CharField(max_length=100, default='not defined')
-    code_count = models.IntegerField(default=0)
+    code_key = models.IntegerField(
+                primary_key=True
+            )
+    code_name = models.CharField(
+                max_length=100, 
+                default='not defined',
+            )
+    code_count = models.IntegerField(
+                default=0
+            )
+
