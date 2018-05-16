6aa217b2ac1fabee7b10f920a7764b0ba2f0e25e
zzde <zhangxh1997@gmail.com>
2018-05-16 23:36:54 +0800

    fix store goods

--- cms/models.py
+++ cms/models.py
@@ -148,6 +148,8 @@ class Store(models.Model):
         all_store_price = StoreGoods.objects.filter(store=self)
         result = []
         for i in all_store_price:
+            if i.goods.id <= 0:
+                continue
             t_info = i.goods.info()
             t_info['goods_price'] = float(i.goods_price)
             t_info['goods_store_stock'] = i.goods_stock
